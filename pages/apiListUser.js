b8fdc4476fbbf94bed55dea28805da6aa4e7eedd
muhammad firdaus <muhammadfirdaus2458@gmail.com>
2026-02-16 23:11:36 +0700

    Update apiListUser.js

--- pages/apiListUser.js
+++ pages/apiListUser.js
@@ -1,11 +1,23 @@
-import { db } from "../lib/firebase"
+export const config = {
+  runtime: "nodejs",
+}
+
+import { getDb } from "../lib/firebase"
 
 export default async function handler(req, res) {
+  const db = await getDb()
+
   const snap = await db.collection("users").get()
-  const users = snap.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data(),
-    password: undefined
-  }))
+
+  const users = snap.docs.map(doc => {
+    const data = doc.data()
+    return {
+      id: doc.id,
+      username: data.username,
+      role: data.role,
+      createdAt: data.createdAt || null,
+    }
+  })
+
   res.json(users)
 }
