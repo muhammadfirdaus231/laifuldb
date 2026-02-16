ab8f5d5f72afc43552b5a867802c82303f684608
muhammad firdaus <muhammadfirdaus2458@gmail.com>
2026-02-16 23:44:34 +0700

    Update apiListUser.js

--- pages/apiListUser.js
+++ pages/apiListUser.js
@@ -1,11 +1,19 @@
-export const config = {
-  runtime: "nodejs",
-}
-
-import { getDb } from "../lib/firebase"
+export const config = { runtime: "nodejs" }
 
 export default async function handler(req, res) {
-  const db = await getDb()
+  const admin = require("firebase-admin")
+
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      }),
+    })
+  }
+
+  const db = admin.firestore()
 
   const snap = await db.collection("users").get()
 
