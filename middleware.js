23373ea4b62c951be5831ca7e813091d9f1eaad8
muhammad firdaus <muhammadfirdaus2458@gmail.com>
2026-02-16 22:59:18 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server"
-import jwt from "jsonwebtoken"
 
 export function middleware(req) {
   const token = req.cookies.get("token")?.value
@@ -8,31 +7,10 @@ export function middleware(req) {
     return NextResponse.redirect(new URL("/auth/login", req.url))
   }
 
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-      if (
-        req.nextUrl.pathname === "/dasboradmins" &&
-        decoded.role !== "owners"
-      ) {
-        return NextResponse.redirect(new URL("/dasborUser", req.url))
-      }
-
-      if (
-        req.nextUrl.pathname === "/dasborUser" &&
-        decoded.role !== "members"
-      ) {
-        return NextResponse.redirect(new URL("/dasboradmins", req.url))
-      }
-    } catch {
-      return NextResponse.redirect(new URL("/auth/login", req.url))
-    }
-  }
-
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/dasborUser", "/dasboradmins"]
+  matcher: ["/dasborUser", "/dasboradmins"],
+}  matcher: ["/dasborUser", "/dasboradmins"]
 }
