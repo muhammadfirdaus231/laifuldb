edc07b79d3518e4d538a0add0ba98ce2caa07ebd
muhammad firdaus <muhammadfirdaus2458@gmail.com>
2026-02-16 22:35:33 +0700

    Create dasborUser.js

---
+++ pages/dasborUser.js
@@ -0,0 +1,10 @@
+export default function DasborUser() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Dasbor Member</h1>
+        <p>Role: members</p>
+      </div>
+    </div>
+  )
+}
