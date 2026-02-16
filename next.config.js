7ff6ff80fb08f91dd644b519c8920ac3991d7e62
muhammad firdaus <muhammadfirdaus2458@gmail.com>
2026-02-16 23:26:00 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push("firebase-admin")
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
